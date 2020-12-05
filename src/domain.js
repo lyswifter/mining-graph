02ca7ce49d6efa5ad4d4706f1e45b77632fd61fb
lyswifter <liuyong@tianrukj.com>
2020-12-05 22:00:18 +0800

    change domain

--- src/domain.js
+++ src/domain.js
@@ -1,4 +1,4 @@
-const DomainProduction = "http://localhost:9090";
-// const DomainProduction = "http://39.170.24.100:12090";
+// const DomainProduction = "http://localhost:9090";
+const DomainProduction = "http://39.170.24.100:12090";
 
 export default DomainProduction;
