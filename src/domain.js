fac27efa0672eb7c54261551b1a63b8a2f9de146
lyswifter <liuyong@tianrukj.com>
2020-12-10 10:47:16 +0800

    change domain api

--- src/domain.js
+++ src/domain.js
@@ -1,4 +1,5 @@
 // const DomainProduction = "http://localhost:9090";
-const DomainProduction = "http://39.170.24.100:12090";
+// const DomainProduction = "http://39.170.24.100:12090";
+const DomainProduction = "http://39.170.24.100:21089";
 
 export default DomainProduction;
